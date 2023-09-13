4806deab30ea5813a24770ca6673b2b0ac514955
Denis <hopeelove2002@gmail.com>
2023-09-13 21:58:51 +0300

    Web app 1

--- java/java.js
+++ java/java.js
@@ -1,4 +1,22 @@
-function c(arggggg) {
-    console.log(arggggg);
+function c(arg) {
+    console.log(arg);
 }
 
+//________________________________________________________________________
+
+numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", "");
+
+personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+personalMovieDB['movies'] = {
+    lastFilm: prompt ("Один из последних просмотренных фильмов?", ""),
+    gradeFilm: prompt ("На скольно вы его оцените?", "")
+};
+
+console.log(personalMovieDB);
