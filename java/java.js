6ec1b5358386a20786dd62d23178b5fb5a65b219
Denis <hopeelove2002@gmail.com>
2023-09-20 22:58:37 +0300

    event listener

--- java/java.js
+++ java/java.js
@@ -6,50 +6,43 @@ function c(arg) {
 
 //________________________________________________________________________
 
-const box = document.getElementById("box"),
-      btns = document.getElementsByTagName("button"),
-      circles = document.getElementById("circles"),
-      oneCircle = circles.querySelectorAll(".circle");
+// const btns = document.querySelectorAll("button"),
+//       overlay = document.querySelector(".overlay")
 
-// box.style.backgroundColor = "blue";      //1-й вариант
-// box.style.width = "500px";
-// box.style.borderRadius = "0%";
-// box.style.cssText = 'bacgrond-color: red; width: 500px; border-radius: 0%;'     //2-ой вариант
-
-// btns[1].style.borderRadius = "100%";
-
-// circles[0].style.backgroundColor = "red"; // отдельного элемента в массиве
-
-// for (let i = 0; i < circles.length; i++) {      //Цикл для задания стилей нескольким элементам, взятым из массива (старый  пример)
-//     circles[i].style.backgroundColor = "grey";
+// btn.onclick = () => {        //Старый формат
+//   alert("hi");
 // }
 
-// circles.forEach (item => {
-//     item.style.backgroundColor = "grey";
+// btn.addEventListener('click', () => {         //Современный вариант
+//   alert('hi');
+// })
+
+// const deleteEvent = (event) => {         //Современный вариант
+//   let dell = false;
+
+//   c(event.target);
+//   dell = true;
+//   if (dell) {
+//     btn.removeEventListener('click', deleteEvent);
+//   }
+// };
+
+// const deleteEvent = (event) => {
+//   c(event.target);
+//   c(event.type);
+  
+// };
+
+// btns.forEach(item => {
+//   item.addEventListener('click', deleteEvent, {once: true});        //Назначение обработчика событий на все кнопки внутри массива и установка из работы 1 раз
 // });
 
-// const text = document.createTextNode("hi");
-const div = document.createElement("div");
 
-div.classList.add('black');
+// overlay.addEventListener('click', deleteEvent)    //сплытие событий
 
-// document.querySelector("#circles").append(div);
-circles.append(div);        //В конец
-// circles.prepend(div);        //В начало
+// const link = document.querySelector('a');
 
-// oneCircle[1].before(div);       //Для более точной вставки (Yачало)
-// oneCircle[1].after(div);       //Для более точной вставки (Конец)
-
-// oneCircle[0].remove();       //Удаление элемента
-
-// oneCircle[0].replaceWith(btns[0]); //Меняет местами элементы
-
-// div.textContent = "hallo"       //Только текст
-
-div.innerHTML = "<h1>HI</h1>" ;         //непосредственно в HTML документ
-
-div.insertAdjacentHTML("afterend", "<h2>Hallo</h2>");
-// beforbegin - перед элементом div
-// afterbegin - внутри элемента div, в начало
-// beforeend - внутри элемента div, в конец
-// afterend - после элемента div
+// link.addEventListener('click', (event) => {
+//   event.preventDefault();       //Отключает стандартное поведение браузера
+//   c('Вы перешли в ютуб');
+// });
