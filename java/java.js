3ee4a46b9875c0076bcb384de493b4bb0814f5f5
Denis <hopeelove2002@gmail.com>
2023-09-20 10:49:55 +0300

    work with el in HTML

--- java/java.js
+++ java/java.js
@@ -1,25 +1,55 @@
-'use strict';
+"use strict";
 
 function c(arg) {
-    console.log(arg);
+  console.log(arg);
 }
 
 //________________________________________________________________________
 
-// const box = document.getElementById('box');
+const box = document.getElementById("box"),
+      btns = document.getElementsByTagName("button"),
+      circles = document.getElementById("circles"),
+      oneCircle = circles.querySelectorAll(".circle");
 
-// c(box);
+// box.style.backgroundColor = "blue";      //1-й вариант
+// box.style.width = "500px";
+// box.style.borderRadius = "0%";
+// box.style.cssText = 'bacgrond-color: red; width: 500px; border-radius: 0%;'     //2-ой вариант
 
-// const btns = document.getElementsByTagName('button')
+// btns[1].style.borderRadius = "100%";
 
-// c(btns);
+// circles[0].style.backgroundColor = "red"; // отдельного элемента в массиве
 
-// const buttons = document.querySelectorAll(".buttons");
+// for (let i = 0; i < circles.length; i++) {      //Цикл для задания стилей нескольким элементам, взятым из массива (старый  пример)
+//     circles[i].style.backgroundColor = "grey";
+// }
 
-// buttons.forEach(item => {
-//     c(item);
+// circles.forEach (item => {
+//     item.style.backgroundColor = "grey";
 // });
 
-// const buttons = document.querySelector(".buttons");
+// const text = document.createTextNode("hi");
+const div = document.createElement("div");
 
-// c(buttons);
+div.classList.add('black');
+
+// document.querySelector("#circles").append(div);
+circles.append(div);        //В конец
+// circles.prepend(div);        //В начало
+
+// oneCircle[1].before(div);       //Для более точной вставки (Yачало)
+// oneCircle[1].after(div);       //Для более точной вставки (Конец)
+
+// oneCircle[0].remove();       //Удаление элемента
+
+// oneCircle[0].replaceWith(btns[0]); //Меняет местами элементы
+
+// div.textContent = "hallo"       //Только текст
+
+div.innerHTML = "<h1>HI</h1>" ;         //непосредственно в HTML документ
+
+div.insertAdjacentHTML("afterend", "<h2>Hallo</h2>");
+// beforbegin - перед элементом div
+// afterbegin - внутри элемента div, в начало
+// beforeend - внутри элемента div, в конец
+// afterend - после элемента div
