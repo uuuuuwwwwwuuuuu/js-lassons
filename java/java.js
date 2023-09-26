d0946d1f9150ab22ee9be9664664ee2384c741aa
Denis <hopeelove2002@gmail.com>
2023-09-26 20:56:55 +0300

    Data

--- java/java.js
+++ java/java.js
@@ -4,37 +4,14 @@ function c(arg) {
     console.log(arg);
 }
 
-//________________________________ Delegirovanie ________________________________________
+//________________________________ Work with data (time) ________________________________________
 
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector(".btn-block");
+const now = new Date();
 
-// c(btns[0].classList.length);
-// c(btns[0].classList.item(0));
-// btns[1].classList.add('red');
-// btns[0].classList.remove('blue');
-// btns[0].classList.toggle('blue');
+// c(now.getHours());
+// now.getDate();                      //День
+// now.getFullYear();                  //Год
+// now.getHours();                     //Час
+// now.getMonth();                     //Месяц      (Начинается с нуля)
+// now.getDay();                       //День       (Начинается с воскресенья, с нуля)
 
-// if (btns[1].classList.contains('red')) {
-//     c('red');
-// }
-
-// btns[0].addEventListener('click', () => {
-//     if (!btns[1].classList.contains('red')) {
-//         btns[1].classList.add('red');
-//     } else {
-//         btns[1].classList.remove('red');
-//     }
-// });
-
-// console.log(btns[0].className);
-
-wrapper.addEventListener('click', (event) => {
-    if (event.target.tagName == 'BUTTON' && event.target) {
-        c('Hallo');
-    }
-});
-
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn); 
