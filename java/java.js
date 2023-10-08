f2a82a5061d23702696c64b02b721b948c7126d2
Denis <hopeelove2002@gmail.com>
2023-10-08 11:27:45 +0300

    myTask

--- java/java.js
+++ java/java.js
@@ -4,80 +4,45 @@ const c = (arg) => {
     console.log(arg);
 }
 
-//________________________________________________________________________
+//_________________________________ Task recursia _______________________________________
 
-const personalMovieDB = {
-    count: null,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: true,
-    start: () => {
-        personalMovieDB["count"] = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    
-        while (personalMovieDB["count"] == '' || personalMovieDB["count"] == null || isNaN(personalMovieDB["count"])) {
-            personalMovieDB["count"] = +prompt("Сколько фильмов вы уже посмотрели?", "");
-        }
-    },
-    rememberMyFilms: () => {
-        for (let i = 0; i < 2; i++) {
-            const a = prompt ("Какой последний фильм вы смотрели?", ""),
-                  b = prompt ("На скольно вы его оцените?", "");
-    
-            if (a != null && b != null && a != '' && b != '' && a.length <= 50) {
-                personalMovieDB.movies[a] = b;
-                c("done");
-            } else {
-                c("error");
-                i--;
+const animals = {
+    cats: {
+        cat1: {
+            name: 'Barsic',
+            age: 7,
+            childrens: {
+                kitten1: {
+                    name: 'Myrka',
+                    age: 2
+                }
             }
-        }
-    },
-    detectPersonalLevel: () => {
-        if (personalMovieDB.count < 10) {
-            c("Просмотренно слишком мало фильмов");
-        } else if (personalMovieDB.count >= 10 && personalMovieDB.count <= 30) {
-            c("Вы классический зритель");
-        } else if (personalMovieDB.count > 30) {
-            c("Вы киноман");
-        } else {
-            c("Произошла ошибка");
-        }
-    },
-    showMyDB: () => {
-        if (!personalMovieDB.privat) {
-            console.log(personalMovieDB);
-        }
-    },
-    writeYourGenres: () => {
-        for (let i = 0; i < 3; i++) {
-            let a = i + 1;
-             let b = prompt(`Ваш любимый жанр №${a}`, '');
-
-            if (b != null && b.trim() != '') {
-                personalMovieDB.genres[i] = b;
-            } else {
-                i--;
-            }
-        }
-        personalMovieDB.genres.forEach((item, i) => {
-            const x = i + 1;
-            c(`Любимый жанр ${x} - это ${item}`);
-        })
-    },
-    toggeleVisibleMyDB: (obj) => {
-        if (obj.privat) {
-            obj.privat = false;
-        } else {
-            obj.privat = true;
+        },
+        cat2: {
+            name: 'Luna',
+            age: 3
         }
     }
 };
+// debugger
+function sumAge(data) {
+    if (!(typeof(data) == 'object')) {
+        let total = 0;
+        if (!(typeof(data) == 'string')) {
 
-personalMovieDB.start();
-personalMovieDB.rememberMyFilms();
-personalMovieDB.detectPersonalLevel();
-personalMovieDB.writeYourGenres();
-personalMovieDB.toggeleVisibleMyDB(personalMovieDB);
-personalMovieDB.showMyDB();
+            total += data;
+        }  
+        return total;
+    } else {
+        let total = 0;
+        
+        for (let key of Object.values(data)) {
+            const subData = sumAge(key);
 
+            total += subData;
+        }
+        return total;
+    }
+}
+
+c(sumAge(animals));
